6964e14d77333d7dc81f07f198c7518977640c0e
Julian Hernandez <j.hernandez.bo@gmail.com>
2020-04-27 14:28:49 -0500

    Agrego pagina de inicio Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3] });
+  res.render('index', { title: 'Solumaster'});
 });
+
 
 module.exports = router;
