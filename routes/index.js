c3424c6db3bd337fb4ae1c0805c1f9fb6ea84054
Saguirre9911 <57844338+Saguirre9911@users.noreply.github.com>
2020-04-27 18:40:28 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Solumaster'});
+  res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3] });
+});
+router.get('/productos', function(req, res, next) {
+  res.render('productos/productos');
+});
+router.get('/compras', function(req, res, next) {
+  res.render('inndex');
+});
+router.get('/nosotros', function(req, res, next) {
+  res.render('index', { title: 'Nosotros', condition: true, anyArray: [1,2,3] });
+});
+router.get('/usuarios', function(req, res, next) {
+  res.render('usuarios/ingresar');
 });
 
+router.get('/usuarios/registro', function(req, res, next) {
+  res.render('usuarios/registro');
+});
+
+router.get('/usuarios/validar', function(req, res, next) {
+  res.render('usuarios/validacion');
+});
+
+
+router.post('/usuario/submit', function(req, res, next) {
+  res.redirect('/usuario/registro');
+});
 
 module.exports = router;
